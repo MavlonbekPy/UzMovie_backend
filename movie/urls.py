4226cfe00396056c9eb73321ef4e1b1a2d2dea3e
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-05-18 18:15:05 +0500

    Movie views.py sorted

--- movie/urls.py
+++ movie/urls.py
@@ -1,5 +1,20 @@
 from django.urls import path
+from .views import MovieViewSet, CommentViewSet
 
 urlpatterns = [
+    # MOVIE
+    path('movie/get-all-movies/', MovieViewSet.as_view({'get': 'get_all'})),
+    path('movie/get-one/', MovieViewSet.as_view({'get': 'get_one'})),
+    path('movie/by-genre/', MovieViewSet.as_view({'get': 'get_by_genre'})),
+    path('movie/by-director/', MovieViewSet.as_view({'get': 'get_by_director'})),
+    path('movie/by-actor/', MovieViewSet.as_view({'get': 'get_by_actor'})),
+    # SAVED
+    path('movie/save/', MovieViewSet.as_view({'post': 'save_movie'})),
+    path('movie/delete/', MovieViewSet.as_view({'delete': 'delete_movie'})),
+
+    # COMMENT
+    path('comment/create/', CommentViewSet.as_view({'post': 'create'})),
+    path('comment/update/', CommentViewSet.as_view({'patch': 'update'})),
+    path('comment/delete/', CommentViewSet.as_view({'delete': 'delete'})),
 
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from movie.models import Movie
+
+
+class MovieViewSet(ViewSet):
+    def get_all(self, request):
+        pass
+
+    def get_one(self, request):
+        pass
+
+    def get_by_genre(self, request):
+        pass
+
+    def get_by_director(self, request):
+        pass
+
+    def get_by_actor(self, request):
+        pass
+
+    def save_movie(self, request):
+        pass
+
+    def delete_movie(self, request):
+        pass
+
+
+class CommentViewSet(ViewSet):
+    def create(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def delete(self, request):
+        pass
