120facf88c0a1583eb6068d7b5b07ad9d5029265
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-05-18 15:34:21 +0500

    models almost finished

--- movie/models.py
+++ movie/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.conf import settings
-
 from authentication.models import MyUser
 
 
@@ -34,11 +32,6 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Actor)
     directors = models.ForeignKey(Director, on_delete=models.CASCADE)
     imdb_rating = models.FloatField()
-    imdb_votes = models.IntegerField(default=0)
-    imdb_votes_average = models.FloatField(default=0)
-    imdb_ratings_average = models.FloatField(default=0)
-    imdb_ratings_count = models.IntegerField(default=0)
-    imdb_votes_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
@@ -48,9 +41,6 @@ class Saved(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.movie
 
 
 class Comment(models.Model):
@@ -64,14 +54,8 @@ class Comment(models.Model):
 
     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.user
-
 
 class Transaction(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user
